0b2990796333db53079e835ce7533b0a89a33ca2
khaine <cucaithuivl@gmail.com>
2026-05-12 20:11:03 +0700

    big update

--- js/lib.js
+++ js/lib.js
@@ -1,6 +1,22 @@
-﻿const list[
-    {id:"001",name:"hoa",gia:500000}
-];
-function add(){
-   
+﻿function add(){
+
+    const Divboss = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = "../assets/images/hoa.jpg";
+    img.width = 600;
+    Divboss.appendChild(img)
+
+    const Div2 = document.createElement("div");
+    const para = document.createElement("p");
+	const textp = document.createTextNode("hoa hong ne");
+	para.appendChild(textp);
+	Div2.appendChild(para);
+
+	const para1 = document.createElement("p");
+	const textp1 = document.createTextNode("gia 2 diamond");
+	para1.appendChild(textp1);
+	Div2.appendChild(para1);
+    Divboss.appendChild(Div2);
+
+    document.body.appendChild(Divboss);
 }
